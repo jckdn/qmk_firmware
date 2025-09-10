49b63236bcf9ed7470884efcf7c11a2d0f0e1648
jckdn <jdean3@live.co.uk>
2025-09-10 16:59:40 +0100

    misc
    * enable permissive hold to aid chordal
    * adjust tap term
    * adjust tap term for backspace
    * combo fix
    * comments & formatting

--- keyboards/planck/keymaps/winux-36/config.h
+++ keyboards/planck/keymaps/winux-36/config.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#define TAPPING_TERM 200
+#define TAPPING_TERM 300
 #define TAPPING_TERM_PER_KEY
 
 #define COMBO_TERM 30
 // #define COMBO_TERM 1000 // useful to test that new fw was flashed
 
-// #define PERMISSIVE_HOLD
+#define PERMISSIVE_HOLD
 // #define HOLD_ON_OTHER_KEY_PRESS
 
 #define CHORDAL_HOLD
