9ebd911b078dc90232b0102d8efc08f0fba81cb8
Jack Dean <jack.dean@myenergi.com>
2026-05-22 16:22:08 +0100

    align mac config with winux

--- keyboards/planck/keymaps/mac-36/config.h
+++ keyboards/planck/keymaps/mac-36/config.h
@@ -1,12 +1,14 @@
 #pragma once
 
-#define TAPPING_TERM 200
+#define TAPPING_TERM 300
 #define TAPPING_TERM_PER_KEY
 
 #define COMBO_TERM 30
+#define COMBO_TERM_PER_COMBO
+#define COMBO_STRICT_TIMER // added to see if helps with accidental enter fires, but maybe just tweaking the timeout is good enough.
 // #define COMBO_TERM 1000 // useful to test that new fw was flashed
 
-// #define PERMISSIVE_HOLD
+#define PERMISSIVE_HOLD
 // #define HOLD_ON_OTHER_KEY_PRESS
 
 #define CHORDAL_HOLD
